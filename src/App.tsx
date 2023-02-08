e42140657bd8c2dd52836252437a5cdc3b59ee38
Humed-Muhammad <abc45171@gmail.com>
2023-02-08 22:17:26 +0300

    ✨ Adding animation to some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { Button, Container, Flex, useToast } from '@chakra-ui/react';
+import {
+  Button,
+  Card,
+  Container,
+  Flex,
+  Link,
+  useToast,
+} from '@chakra-ui/react';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { actions as defaultActions } from './store/defaultSlice/slice';
@@ -7,7 +14,17 @@ import {
   selectConnectingWallet,
   selectMessage,
 } from './store/defaultSlice/slice/selector';
-import { ethereum } from './utils/constant';
+import {
+  ethereum,
+  FADE_DOWN_ANIMATION_VARIANTS,
+  FADE_UP_ANIMATION_VARIANTS,
+} from './utils/constant';
+import { motion } from 'framer-motion';
+import LoadingDots from './components/Core/icons/loading-dots';
+import Balancer from 'react-wrap-balancer';
+import { Layers } from 'react-feather';
+import { Layout } from './Layout';
+import { ChakraTable } from './components/Core/ChakraTable';
 
 function App() {
   const toast = useToast();
@@ -25,6 +42,7 @@ function App() {
 
   useEffect(() => {
     if (connectedAccount) {
+      console.log('first');
       dispatch(defaultActions.requestContract());
     }
   }, [connectedAccount]);
@@ -35,28 +53,28 @@ function App() {
         case 'error':
           toast({
             description: message.content,
-            variant: 'left-accent',
+            variant: 'subtle',
             status: 'error',
           });
           break;
         case 'success':
           toast({
             description: message.content,
-            variant: 'left-accent',
+            variant: 'subtle',
             status: 'success',
           });
           break;
         case 'warning':
           toast({
             description: message.content,
-            variant: 'left-accent',
+            variant: 'subtle',
             status: 'warning',
           });
           break;
         case 'info':
           toast({
             description: message.content,
-            variant: 'left-accent',
+            variant: 'subtle',
             status: 'info',
           });
           break;
@@ -72,23 +90,72 @@ function App() {
     };
   }, [message.content]);
   return (
-    <Flex direction="column" justify="center" align="center" height={['100vh']}>
-      <Button
-        colorScheme="blue"
-        my={[3]}
-        isLoading={connectingWallet}
-        loadingText="Connecting to wallet..."
-        onClick={() => {
-          if (!connectedAccount) {
-            dispatch(defaultActions.requestWalletConnection());
-          }
+    <Layout>
+      <motion.div
+        className="max-w-xl px-5 xl:px-0"
+        initial="hidden"
+        whileInView="show"
+        animate="show"
+        viewport={{ once: true }}
+        variants={{
+          hidden: {},
+          show: {
+            transition: {
+              staggerChildren: 0.15,
+            },
+          },
         }}
-        variant="outline"
       >
-        {connectedAccount ? 'Connected' : 'Connect your wallet'}
-      </Button>
-      <Container>{/* <ChakraTable /> */}</Container>
-    </Flex>
+        <motion.h1
+          className="font-display bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center text-4xl font-bold font-bold tracking-[-0.02em] text-transparent drop-shadow-sm md:text-6xl md:leading-[5rem]"
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+        >
+          <Balancer>Create your Desired Blockchain Roles</Balancer>
+        </motion.h1>
+        <motion.p
+          className="relative mt-6 text-center text-gray-500 md:text-xl"
+          variants={FADE_UP_ANIMATION_VARIANTS}
+        >
+          <Balancer>
+            Roles Such as <span className="text-cyan-500">Create</span>,{' '}
+            <span className="text-cyan-500">Read</span>,{' '}
+            <span className="text-cyan-500">Update, etc...</span> With Ease.
+          </Balancer>
+        </motion.p>
+        <motion.div
+          className="mx-auto mt-6 flex items-center justify-center space-x-5"
+          variants={FADE_UP_ANIMATION_VARIANTS}
+        >
+          <Button
+            onClick={() => {
+              if (!connectedAccount) {
+                dispatch(defaultActions.requestWalletConnection());
+              }
+            }}
+            disabled={connectingWallet}
+            type="submit"
+            width={['full', 60]}
+          >
+            {connectingWallet ? (
+              <LoadingDots />
+            ) : connectedAccount ? (
+              'Wallet is Connected'
+            ) : (
+              'Connect your wallet'
+            )}
+          </Button>
+        </motion.div>
+      </motion.div>
+
+      <motion.div
+        variants={FADE_DOWN_ANIMATION_VARIANTS}
+        className="my-5 flex h-96 w-11/12 animate-[slide-down-fade_0.5s_ease-in-out] flex-col items-center md:w-2/3 xl:px-0"
+      >
+        <Card width={['full']}>
+          <ChakraTable />
+        </Card>
+      </motion.div>
+    </Layout>
   );
 }
 
