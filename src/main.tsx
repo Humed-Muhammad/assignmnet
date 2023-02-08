68ce6b5162ad4825fb2cb08809c690114468f6ec
Humed-Muhammad <abc45171@gmail.com>
2023-02-08 22:17:37 +0300

    Adding tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
