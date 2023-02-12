7dbb37ef5dfc7d7d4f1671b0133136b3e1e36647
Humed-Muhammad <abc45171@gmail.com>
2023-02-12 20:49:37 +0300

    ✨ Adding core component for tooltip

---
+++ src/components/Core/Tooltip.tsx
@@ -0,0 +1,21 @@
+import { Tooltip, TooltipProps } from "@chakra-ui/react";
+import React from "react";
+
+interface Props extends TooltipProps {
+  children: JSX.Element[] | JSX.Element;
+}
+export const TooltipHolder = ({ children, ...rest }: Props) => {
+  return (
+    <Tooltip
+      textAlign="center"
+      fontSize="sm"
+      py="1"
+      bg="gray.600"
+      placement="top-start"
+      {...rest}
+      hasArrow
+    >
+      {children}
+    </Tooltip>
+  );
+};
