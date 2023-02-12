837d633c2ab5dfe038bca8244e5a11dfafe9e34e
Humed-Muhammad <abc45171@gmail.com>
2023-02-12 17:41:12 +0300

    🎨 Helper for Fetching smart contract data

--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -1,53 +1,40 @@
-import { BigNumber, Contract, ethers, Signer } from 'ethers';
+import { BigNumberish, Contract, ethers, Signer } from 'ethers';
+import { IGetContractDataTypes } from 'src/store/defaultSlice/slice/types';
 import MemberRole from '../abi/MemberRole.json';
 
 export interface GetLotteryResponseType {
   signer: Signer;
   connectedAccount: string;
-  // gasPrice: number;
-  gasLimit: number;
 }
 
-// export const formatEther = (value: BigNumber | undefined) => {
-//   if (value) {
-//     const result = ethers.formatEther(value);
-//     return result;
-//   }
-// };
+export const formatEther = (value: BigNumberish | undefined) => {
+  if (value) {
+    const result = ethers.utils.formatEther(value);
+    return result;
+  }
+};
 
-export const getLotteryData = async (data: GetLotteryResponseType) => {
+export const getSmartContractData = async (
+  data: GetLotteryResponseType,
+): Promise<IGetContractDataTypes> => {
   const contract: Contract = new ethers.Contract(
-    import.meta.env.VITE_CONTRACT_ADDRESS,
+    import.meta.env.VITE_LOCAL_CONTRACT_ADDRESS,
     MemberRole.abi,
     data.signer,
   );
 
-  // console.log(
-  //   import.meta.env.VITE_CONTRACT_ADDRESS,
-  //   MemberRole.abi,
-  //   data.signer,
-  //   data.connectedAccount,
-  // );
-  const transactionOptions = {
-    gasLimit: 6000000,
-    gasPrice: ethers.utils.parseUnits('1.0', 'gwei'),
-    // value: ethers.utils.parseEther('3.0'), // adding this should fix
-  };
-  // const roles = await contract?.addRoleType('CreateRole');
-  const addresses = await contract?.roleTypes(
-    data.connectedAccount,
-    transactionOptions,
-  );
+  const membersCount = await contract?.membersCount();
+  const roleTypesCount = await contract?.roleTypesCount();
 
-  // const rolesTypes = await contract?.roleTypes(data.connectedAccount, {
-  //   gasPrice: 22,
-  //   gasLimit: 6385876,
-  // });
-  // const owner = await contract?.owner();
-  console.log(addresses);
+  const roles = await contract?.getAllRoleTypes();
+  const addresses = await contract?.getAllAdressess();
+  console.log(roles, addresses);
 
   return {
-    // owner,
     contract,
+    membersCount: membersCount.toNumber(),
+    roles,
+    usersAddress: addresses,
+    roleTypesCount: roleTypesCount.toNumber(),
   };
 };
