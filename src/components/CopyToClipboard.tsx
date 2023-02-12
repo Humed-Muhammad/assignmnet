2f41009be7e640a42bddbb612aa5d81c4f209416
Humed-Muhammad <abc45171@gmail.com>
2023-02-12 20:47:06 +0300

    ✨ Copy to clipboard component

---
+++ src/components/CopyToClipboard.tsx
@@ -0,0 +1,27 @@
+import { IconButton } from '@chakra-ui/react';
+import React from 'react';
+import { useCopyToClipboard } from '../utils/hooks';
+
+import { Copy } from 'react-feather';
+import { Check } from 'react-feather';
+import { TooltipHolder } from './Core/Tooltip';
+interface Props {
+  value: string | undefined;
+}
+export const CopyToClipboard = ({ value }: Props) => {
+  const { copyToClipboard, copyed } = useCopyToClipboard();
+  return (
+    <TooltipHolder label="Copy to clipboard">
+      <IconButton
+        onClick={() => {
+          copyToClipboard(value);
+        }}
+        bg="transparent"
+        color="gray.600"
+        rounded="full"
+        aria-label="copy to clipboard"
+        icon={copyed ? <Check size={20} /> : <Copy size={20} />}
+      />
+    </TooltipHolder>
+  );
+};
