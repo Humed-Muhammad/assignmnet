8fd1b5a4e53888b762bfe6ff3a6c2159c054f8e0
Humed-Muhammad <abc45171@gmail.com>
2023-02-12 20:48:40 +0300

    🎨 IMproving chakra modal

--- src/components/ChakraModal.tsx
+++ src/components/ChakraModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   Button,
+  Center,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -8,7 +9,9 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Text,
 } from '@chakra-ui/react';
+import LoadingDots from './Core/icons/loading-dots';
 
 interface Props {
   isOpen: boolean;
@@ -16,18 +19,22 @@ interface Props {
   title: string;
   children: React.ReactNode | React.ReactNode[];
   command?: () => void;
+  loading?: boolean;
+  actionText?: string;
 }
 
 export function ChakraModal({
   isOpen,
+  actionText = 'Create',
   onClose,
   title,
   children,
   command,
+  loading,
 }: Props) {
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isCentered isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>{title}</ModalHeader>
@@ -35,8 +42,18 @@ export function ChakraModal({
           <ModalBody pb={6}>{children}</ModalBody>
 
           <ModalFooter>
-            <Button onClick={command} colorScheme="blue" mr={3}>
-              Create
+            <Button
+              onClick={command}
+              colorScheme={loading ? 'gray' : 'green'}
+              mr={3}
+            >
+              {loading ? (
+                <Center>
+                  <LoadingDots />
+                </Center>
+              ) : (
+                actionText
+              )}
             </Button>
             <Button onClick={onClose}>Cancel</Button>
           </ModalFooter>
